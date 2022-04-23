d30d4847d1d7b085011cf1e1be8da8adb483f536
Guilherme Pedrosa <guilherme@gtpedrosa>
2022-04-23 10:00:36 -0300

    remove conflicting roam key bindings

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -81,14 +81,7 @@
       :hook
       (after-init . org-roam-mode)
       :custom
-      (org-roam-directory "~/Dropbox/orgs/roam/")
-      :bind (:map org-roam-mode-map
-              (("C-c n l" . org-roam)
-               ("C-c n f" . org-roam-find-file)
-               ("C-c n b" . org-roam-switch-to-buffer)
-               ("C-c n g" . org-roam-graph-show))
-              :map org-mode-map
-              (("C-c n i" . org-roam-insert))))
+      (org-roam-directory "~/Dropbox/orgs/roam/"))
 
 ;; Fix upstream issue with emacs
 (setq internal-lisp-face-attributes
